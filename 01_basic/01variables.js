a49d2e9098ab2e044763b6076fb2cb3f787c919b
Ritesh28082002 <131480371+Ritesh28082002@users.noreply.github.com>
2025-02-14 19:19:21 +0000

    container config done

---
+++ 01_basic/01variables.js
@@ -0,0 +1,8 @@
+const accountId  = 144553;
+let accountemail = "ritesh12@gmail.com";
+var accountPassword = '1233445';
+accountCity = "Jaipur";
+
+console.log(accountId);
+console.table([accountId, accountemail, accountPassword, accountCity]);
+                                                                                                                                                                                                                                                                                                

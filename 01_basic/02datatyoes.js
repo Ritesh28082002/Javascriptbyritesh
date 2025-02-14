f30716266015f6d71e400274e3c4a81a6ca7d8f1
Ritesh28082002 <131480371+Ritesh28082002@users.noreply.github.com>
2025-02-14 19:43:37 +0000

    datatypes

---
+++ 01_basic/02datatyoes.js
@@ -0,0 +1,7 @@
+"use strict";//treat all as js code as new version 
+alert ("hello");//pop up message
+let name = "ritesh ";
+let age = "18";
+let isloggedin = false;
+//datatypes are :-
+// number,bigint,string,null,undefuned,symbol,these are primitive datatypes

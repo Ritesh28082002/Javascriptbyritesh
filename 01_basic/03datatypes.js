446f174c3ee3a22aa7604b1be6a461295fdff7df
Ritesh28082002 <131480371+Ritesh28082002@users.noreply.github.com>
2025-02-15 07:25:05 +0000

    datatypes

---
+++ 01_basic/03datatypes.js
@@ -0,0 +1,10 @@
+let score =33
+console.log(typeof score);
+console.log(typeof(score));
+console.log(score);
+// "33" => 33
+// "33abc"=>NAN means not a number
+// true = 1; false => 0 output dega 
+let value = 3 ;
+let negvalue = -value;
+console.log(negvalue); 
